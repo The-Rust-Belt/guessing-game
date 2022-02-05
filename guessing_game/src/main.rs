03bf94426f7d94860abebd6a77bd4360a7491a1f
jpcaulfi <joecaulfield29@yahoo.com>
2022-02-04 23:25:45 -0500

    first example

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,10 +4,13 @@
 //  Check that the input is as expected
 
 use std::io;
+use rand::Rng;
 
 fn main() {
 
     println!("Guess a number:");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read line");
     println!("Your guess: {}", guess);
