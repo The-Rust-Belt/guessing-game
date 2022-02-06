db70750501145db5ecad798515e35fb394904ade
jpcaulfi <joecaulfield29@yahoo.com>
2022-02-06 08:12:50 -0500

    concepts

--- concepts/src/main.rs
+++ concepts/src/main.rs
@@ -55,3 +55,32 @@ fn enum_example() {
         Direction::Down => println!("Down"),
     }
 }
+
+// Structs
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn initialize_struct() {
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+}
+
+// When params have the same name you can just pass them!
+fn initialize_struct_simple(email: String, username: String) -> User {
+    User {
+        email,
+        // email: email,
+        username,
+        // username: username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
