79555a1565b751a2e1addc6eea9e82f80ed8d45a
jpcaulfi <joecaulfield29@yahoo.com>
2022-02-05 22:45:29 -0500

    concepts

---
+++ concepts/src/main.rs
@@ -0,0 +1,42 @@
+fn main() {
+    let x = 5;
+    let x = x + 3;
+    let x = x * 2;
+    println!("{}", x);
+
+    // Can't mutate a variable's type
+    let some_string = "joe";
+    let some_string = some_string.len();
+    let some_other_string = "joe";
+    some_other_string = some_other_string.len();
+}
+
+fn another_function() {
+    println!("Another function")
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn conditional() {
+    let number = 3;
+
+    if number < 5 {
+        println!("True")
+    } else if number == 5 {
+        println!("OK")
+    } else {
+        println!("False")
+    }
+
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break {
+                counter * 2
+            };
+        }
+    };
+    println!("{}", result)
+}
