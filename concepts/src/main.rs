11233510add608ac4dbac24406a70f619cb938f5
jpcaulfi <joecaulfield29@yahoo.com>
2022-02-06 08:21:01 -0500

    concepts

--- concepts/src/main.rs
+++ concepts/src/main.rs
@@ -84,3 +84,30 @@ fn initialize_struct_simple(email: String, username: String) -> User {
         sign_in_count: 1,
     }
 }
+
+fn update_struct() {
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    // Inherit the rest of the values from user1
+    let user2 = User {
+        email: String::from("someoneelse@example.com"),
+        username: String::from("someotherusername123"),
+        ..user1
+    };
+}
+
+// Methods
+struct Person {
+    first_name: String,
+    last_name: String,
+}
+
+impl Person {
+    fn details(&self) -> String {
+        String::from("{}, {}", &self.last_name, &self.first_name)
+    }
+}
