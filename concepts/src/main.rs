b15b3800bb9c03e10bed40543f9f0086d3d3f156
jpcaulfi <joecaulfield29@yahoo.com>
2022-02-06 11:11:41 -0500

    concepts

--- concepts/src/main.rs
+++ concepts/src/main.rs
@@ -138,3 +138,20 @@ fn testing_options() {
     let some_string = Some("a string");
     let absent_number: Option<i32> = Option::None;
 }
+
+// Matching
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        CoinL::Quarter => 25,
+    }
+}
